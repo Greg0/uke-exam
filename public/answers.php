4eff52b04ee0cb7d67b274bc835b64f243cdd44c
Grzegorz Kuźnik <gerg0sz92@gmail.com>
2024-10-22 22:50:07 +0200

    feat: Add answers

---
+++ public/answers.php
@@ -0,0 +1,42 @@
+<?php
+
+use Greg0\UkeExam\CacheableQuestionsQuery;
+use Greg0\UkeExam\QuestionsQuery;
+use OpenSpout\Reader\XLSX\Reader;
+use Symfony\Component\Cache\Adapter\FilesystemAdapter;
+use Twig\Environment;
+use Twig\Loader\FilesystemLoader;
+use Twig\TwigTest;
+
+require_once __DIR__. '/../vendor/autoload.php';
+
+$loader = new FilesystemLoader(__DIR__. '/../templates');
+$twig = new Environment($loader, [
+    'cache' => __DIR__.'/../var/cache',
+]);
+
+$test = new TwigTest('ondisk', function ($value) {
+    return file_exists(__DIR__.$value);
+});
+$twig->addTest($test);
+
+$reader = new Reader();
+$reader->open(__DIR__ . '/UKE_porownanie_pytan.xlsx');
+
+$query = new QuestionsQuery(
+    __DIR__ . '/UKE_porownanie_pytan.xlsx'
+);
+
+$query2 = new CacheableQuestionsQuery(
+    $query,
+    new FilesystemAdapter(directory: __DIR__ . '/../var/cache')
+);
+
+$pageName = $_GET['sheet'] ?? 'nowe';
+$questions = $query2->findAll($pageName);
+
+$template = $twig->load('answers.html.twig');
+echo $template->render([
+    'page' => $pageName,
+    'questions' => $questions,
+]);
